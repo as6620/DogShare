3d845f51061fef489473eb6f9b76a26e0f7c661d
as6620 <152225466+as6620@users.noreply.github.com>
2026-03-12 18:07:14 +0200

    added NetworkReceiver to MasterActivity

--- app/src/main/java/com/example/dogshare/MasterActivity.java
+++ app/src/main/java/com/example/dogshare/MasterActivity.java
@@ -1,4 +1,36 @@
 package com.example.dogshare;
 
-public class MasterActivity {
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MasterActivity extends AppCompatActivity {
+
+    // Variable to hold the network listener
+    private NetworkReceiver networkReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Create the receiver and connect it to this screen
+        networkReceiver = new NetworkReceiver(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Start listening for internet connection changes
+        IntentFilter networkFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(networkReceiver, networkFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Stop listening to save battery and prevent errors
+        if (networkReceiver != null) {
+            unregisterReceiver(networkReceiver);
+        }
+    }
 }
