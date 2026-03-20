1b43e7fd5326fa612f7a27e6bfa0fed3ad3f1493
as6620 <152225466+as6620@users.noreply.github.com>
2026-03-20 13:50:36 +0200

    FeedingFragment Done for specific family, TODO: Implement general family logic and database integration

--- app/src/main/java/com/example/dogshare/Fragments/FeedingFragment.java
+++ app/src/main/java/com/example/dogshare/Fragments/FeedingFragment.java
@@ -1,5 +1,7 @@
 package com.example.dogshare.Fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +9,78 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.dogshare.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FeedingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FeedingFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView[][] feeder = new TextView[7][2];
 
     public FeedingFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FeedingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FeedingFragment newInstance(String param1, String param2) {
-        FeedingFragment fragment = new FeedingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feeding, container, false);
+        View view = inflater.inflate(R.layout.fragment_feeding, container, false);
+
+        feeder[0][0] = view.findViewById(R.id.tV1);
+        feeder[0][1] = view.findViewById(R.id.tV2);
+        feeder[1][0] = view.findViewById(R.id.tV3);
+        feeder[1][1] = view.findViewById(R.id.tV4);
+        feeder[2][0] = view.findViewById(R.id.tV5);
+        feeder[2][1] = view.findViewById(R.id.tV6);
+        feeder[3][0] = view.findViewById(R.id.tV7);
+        feeder[3][1] = view.findViewById(R.id.tV8);
+        feeder[4][0] = view.findViewById(R.id.tV9);
+        feeder[4][1] = view.findViewById(R.id.tV10);
+        feeder[5][0] = view.findViewById(R.id.tV11);
+        feeder[5][1] = view.findViewById(R.id.tV12);
+        feeder[6][0] = view.findViewById(R.id.tV13);
+        feeder[6][1] = view.findViewById(R.id.tV14);
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (feeder[i][j] != null) {
+                    feeder[i][j].setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            changeFeeder(v);
+                        }
+                    });
+                }
+            }
+        }
+
+        return view;
+    }
+
+    public void changeFeeder(View v) {
+        final TextView clickedTV = (TextView) v;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Who's walking the dog?");
+
+        final String[] names = {"Idan", "Yuval", "Adva"};
+
+        builder.setItems(names, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                clickedTV.setText(names[which]);
+            }
+        });
+
+        builder.setNegativeButton("CANCEL", null);
+
+        builder.setNeutralButton("RESET", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                clickedTV.setText("");
+            }
+        });
+
+        builder.show();
     }
 }
